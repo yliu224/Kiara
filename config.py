b985daf2ed01f67b5a5869cee695cc4fb6d5b841
Dean <yliu224@dons.usfca.edu>
2021-01-30 00:11:45 -0800

    Fix the bug where you will fail to select 5 PM at first time

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
 NUMBER_OF_GUESTS = 1
 AFTER_N_DAYS = 3
-TIME = [{'hour':5,'ampm':'PM'},{'hour':4,'ampm':'PM'},{'hour':3,'ampm':'PM'}]
+# Sometimes the PM selection panel won't work at the first time
+# So, we have to book 5pm twice to make sure the selection succeed
+TIME = [{'hour':5,'ampm':'PM'},{'hour':5,'ampm':'PM'},{'hour':4,'ampm':'PM'},{'hour':3,'ampm':'PM'}]
